711557ab34660cc4387c889ad438ec7ed0ca73d3
Vadzim Kazak <it.jrew@gmail.com>
2014-06-05 19:00:51 +0300

    Started work on config loading.

---
+++ src/main/java/com/jrew/lab/patcher/util/ConfigUtil.java
@@ -0,0 +1,17 @@
+package com.jrew.lab.patcher.util;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kazak_VV
+ * Date: 05.06.14
+ * Time: 18:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigUtil {
+
+    //Configuration configuration = new PropertiesConfiguration(filename);
+
+}
